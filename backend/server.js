67461c3185097df0f28644ab615fd385a6a81713
charan-crafts <mangalacharancharan@gmail.com>
2025-11-04 23:24:50 +0530

    First commit

---
+++ backend/server.js
@@ -0,0 +1,45 @@
+require('dotenv').config()
+
+
+const express = require("express")
+
+const app = express()
+
+const cors = require('cors')
+
+app.use(cors())
+app.get("/api/jokes", (req, res) => {
+
+    const jokes = [
+        {
+            "id": 1,
+            "joke": "Why don't programmers like nature? It has too many bugs."
+        },
+        {
+            "id": 2,
+            "joke": "How do you comfort a JavaScript bug? You console it."
+        },
+        {
+            "id": 3,
+            "joke": "Why do Java developers wear glasses? Because they don't C#."
+        },
+        {
+            "id": 4,
+            "joke": "I told my computer I needed a break... and it froze."
+        },
+        {
+            "id": 5,
+            "joke": "What’s a developer’s favorite hangout place? The Foo Bar."
+        }
+    ]
+
+    return res.json(jokes);
+
+})
+
+
+app.listen(process.env.PORT, () => {
+
+    console.log(`Server is running at port ${process.env.PORT}`);
+
+})
---
+++ frontend/src/App.jsx
@@ -0,0 +1,48 @@
+import React, { useEffect } from 'react';
+import { useState } from 'react';
+import axios from 'axios'
+const App = () => {
+
+  const getJokes = async () => {
+
+    const response = await axios.get("/api/jokes")
+
+    console.log(response.data);
+
+    setJokes(response.data)
+
+  }
+
+  useEffect(() => {
+    getJokes();
+  }, [])
+
+  const [jokes, setJokes] = useState([])
+  return (
+    <div className='flex flex-col items-center justify-center p-4 text-2xl mt-5'>
+      <h1 className='text-red-300 font-extralight text-3xl pb-10'>Jokes Application</h1>
+
+      <h2>Available Jokes : {jokes.length}</h2>
+
+      {
+        jokes.length > 0 ?
+          (
+            jokes.map((joke) => {
+              return (
+                <div key={joke.id} className='p-2 text-2xl font-sans'>
+                  <h1>{joke.joke}</h1>
+                </div>
+              )
+            })
+          )
+          :
+          (
+            <h1> No Jookes</h1>
+          )
+      }
+
+    </div>
+  );
+}
+
+export default App;
